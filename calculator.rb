c38cda08ad61288b1b2d2f99f7da176016422db9
Brianna Jordan <bryvjordan9@gmail.com>
2019-07-20 00:04:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+first_number = 0
+while first_number <2 do
+  puts "Hello world #{first_number}!"
+  first_number = first_number + 1
+end
+
+second_number = 1
+while second_number < 2 do
+  puts "Hello world #{second_number}!"
+  second_number = second_number + 1
+end
+
+sum = (first_number + second_number)
+ puts "Hello world #{first_number} + #{second_number}!"
